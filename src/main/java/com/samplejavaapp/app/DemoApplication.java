cfc5e7be68b0a9ecf7165354498dc687423b3dc1
ersel.ekmen <ersel.ekmen@xprclub.com>
2024-07-27 20:57:09 +0300

    mapping added to app controller

--- src/main/java/com/samplejavaapp/app/DemoApplication.java
+++ src/main/java/com/samplejavaapp/app/DemoApplication.java
@@ -1,13 +1,18 @@
 package com.samplejavaapp.app;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-public class DemoApplication {
+@RestController
+public class HomeController {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to the Java App!";
+    }
 
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
 }
